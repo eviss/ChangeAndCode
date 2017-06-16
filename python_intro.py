6d0052536bdba790982497fef2fdf939f7801d98
Evi <eviyler671@gmail.com>
2017-06-16 11:40:08 +0200

    first commit

---
+++ python_intro.py
@@ -0,0 +1,20 @@
+""" name = 'onja'
+if name == 'ola':
+	print('Hey, ola')
+
+elif name == 'onja':
+	print('Hey, onja')
+else:
+	print('Hey, anything')
+
+def hi(name):
+	print('hi ' + name + '!')
+girls = ['ra', 'liliy', 'You']
+for name in girls:
+	hi(name)
+	print('next')
+"""
+
+for i in range(1,7):
+	print(i)
+	
